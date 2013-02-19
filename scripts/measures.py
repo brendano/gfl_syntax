5f027da8fa48e4b48fa18df258179512ff34db8c
Nathan Schneider <nschneid@cs.cmu.edu>
2013-02-19 17:53:17 -0500

    measures.py

--- scripts/measures.py
+++ scripts/measures.py
@@ -13,13 +13,22 @@ from spanningtrees import spanning
 def single_ann_measures(a):
 	c = Counter()
 	c['lexnodes'] = len(a.lexnodes)
+	c['1W'] = sum(1 for n in a.lexnodes if len(n.tokens)==1)
+	c['MW'] = sum(1 for n in a.lexnodes if len(n.tokens)>1)
 	c['coordnodes'] = len(a.coordnodes)
+	c['anaphlinks'] = len(a.anaphlinks)
 	c['FNs'] = len(a.cbbnodes)
 	c['explicitly rooted utterances'] = len(a.root.children)
 	simplify_coord(a)
+	# literal number of connected components in the graph (not counting the root when nothing was explicitly attached to it)
+	c['fragments'] = len({n.frag for n in a.nodes if n.frag.nodes!={a.root}})
+	assert c['fragments']>0,(a.nodes,a.lexnodes)
+	c['max utterances'] = max(1,c['explicitly rooted utterances'])+(c['fragments']-1)
+	c['min utterances'] = max(1,c['explicitly rooted utterances'])
+	assert c['max utterances']>=c['min utterances'],c
 	upward(a)
 	downward(a)
-	c['possibly rooted utterances'] = sum(int(a.root in c.parentcandidates) for c in a.lexnodes)
+	c['possible utterance heads'] = sum(int(a.root in c.parentcandidates) for c in a.lexnodes)
 	return c
 	
 def iaa_measures(a1,a2):
@@ -27,12 +36,15 @@ def iaa_measures(a1,a2):
 
 def main(anns1F, anns2F=None):
 	i = 0
+	a1C, a2C, iaC = Counter(), Counter(), Counter()
 	for ann1ln in anns1F:
 		loc1, sent, ann1JS = ann1ln[:-1].split('\t')
 		ann1J = json.loads(ann1JS)
 		print(i, loc1, '<<', sent)
 		a1 = FUDGGraph(ann1J)
-		print('   ',single_ann_measures(a1))
+		a1single = single_ann_measures(a1)
+		a1C += a1single
+		print('   ',a1single)
 		if anns2F is not None:
 			ann2ln = next(anns2F)
 			loc2, sent2, ann2JS = ann2ln[:-1].split('\t')
@@ -41,9 +53,18 @@ def main(anns1F, anns2F=None):
 			assert len(ann1J['tokens'])==len(ann2J['tokens'])
 			a2 = FUDGGraph(ann2J)
 			print(i, loc2, '>>')
-			print('   ',single_ann_measures(a2))
-			print('   ',iaa_measures(a1,a2))
+			a2single = single_ann_measures(a2)
+			a2C += a2single
+			print('   ',a2single)
+			iaa = iaa_measures(a1,a2)
+			iaC += iaa
+			print('   ',iaa)
 		i += 1
+	print()
+	print(a1C)
+	if a2C:
+		print(a2C)
+		print(iaC)
 
 if __name__=='__main__':
 	anns1F = anns2F = None
